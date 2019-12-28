8177595781ce5c390a3082801b8d228e13861184
spaske00 <spaskeasm@gmail.com>
2019-12-28 17:54:21 +0100

    refacotred main into source code panel

--- main.cpp
+++ main.cpp
@@ -23,20 +23,38 @@
 #include "clang_interface.h"
 namespace fs = std::filesystem;
 
-#include <GL/glew.h> 
-#include <GLFW/glfw3.h>  //must be after opengl
+// About Desktop OpenGL function loaders:
+//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
+//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
+//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>    // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>    // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>  // Initialize with gladLoadGL()
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
+
+// Include glfw3.h after our OpenGL definitions
+#include <GLFW/glfw3.h>
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
 
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
+const static float EDITOR_GRAPH_RATIO = 0.35;
+
 //GraphGui::GraphGui graph(nullptr);
-
-struct imgui
-{
-
-};
 
 namespace gui {
 class MainWindow {
@@ -123,18 +141,13 @@ public:
         glfwTerminate();
     }
 };
-};
 
-
-const static float EDITOR_GRAPH_RATIO = 0.35;  
-
-int main(int, char**)
-{
-    gui::MainWindow main_window;
-    ImGuiIO& io = ImGui::GetIO();
-    io.Fonts->AddFontFromFileTTF("imgui_util/misc/fonts/Cousine-Regular.ttf", 15.0f);
-
-    // Our state
+class SourceCodePanel {
+    ImGuiIO &io;
+    TextEditor editor;
+    MainWindow& main_window;
+    std::string buffer;
+    std::string filename;
     bool is_clicked_NEW = false;
     bool is_clicked_OPEN = false;
     bool bt_Save = false;
@@ -142,31 +155,39 @@ int main(int, char**)
     bool key_event_open = false;
     bool key_event_save = false;
     bool unsaved = true;
-
     bool should_build_callgraph = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    std::string filename;
-
-    TextEditor editor;
-    editor.SetLanguageDefinition(TextEditor::LanguageDefinition::CPlusPlus());
-
-    std::string buffer;
-
-    GraphGui::GraphGui graph(&io, &editor);
-
-
-    while (!glfwWindowShouldClose(main_window.Window()))
+    enum class State
     {
-        glfwPollEvents();
+        Idle, Editing, DoneEditing, OpenCliked, NewCliked, EditCliked
+    } state;
 
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
+public:
+    SourceCodePanel(ImGuiIO& io, MainWindow& main_window) : io(io), main_window(main_window) {
+        editor.SetLanguageDefinition(TextEditor::LanguageDefinition::CPlusPlus());
+    }
+    TextEditor& Editor() { return editor; }
+    auto SecondsSinceLastTextChange() const
+    {
+        return editor.SecondsSinceLastTextChange();
+    }
 
-        ImGui::PushClipRect(ImVec2(100, 100), ImVec2(200, 200), true);
-
+    auto IsTextChanged() const
+    {
+        return editor.IsTextChanged();
+    }
+    bool ShouldBuildCallgraph() const
+    {
+        return should_build_callgraph;
+    }
+    void CallGraphBuilt() {
+        should_build_callgraph = false;
+    }
+    const std::string SourceCode() const
+    {
+        return buffer;
+    }
+    void Update() {
         //*******************
         // KEY EVENTS
         //*******************
@@ -198,42 +219,25 @@ int main(int, char**)
             glfwSetWindowShouldClose(main_window.Window(), GLFW_TRUE);
         }
 
-        float graph_size_x = io.DisplaySize.x*(1 - EDITOR_GRAPH_RATIO)-30;
-        float graph_size_y = io.DisplaySize.y-20;
-        float editor_size_x = io.DisplaySize.x*EDITOR_GRAPH_RATIO;
-        float editor_size_y = io.DisplaySize.y-20;
-        
-        //*******************
-        //GENERATED GRAPH WINDOW
-        //*******************
-        {
-            ImGui::SetNextWindowPos(ImVec2(editor_size_x+25, 10));
-            ImGui::SetNextWindowSize(ImVec2(graph_size_x, graph_size_y));
-            ImGui::Begin("GENERATED CALLGRAPH", __null, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBringToFrontOnFocus);
+    }
 
-            
-            graph.set_window(ImGui::GetCurrentWindow());
-            //PushClipRect(const ImVec2& clip_rect_min, const ImVec2& clip_rect_max, bool intersect_with_current_clip_rect);
-            graph.draw();
-            //graph.focus_node(std::string("base::base()"));
-
-            ImGui::End();
-            ImGui::PopClipRect();
-        }
+    void Draw() {
 
         //*******************
         //SOURCE CODE WINDOW
         //*******************
-        {   
+        float editor_size_x = io.DisplaySize.x*EDITOR_GRAPH_RATIO;
+        float editor_size_y = io.DisplaySize.y-20;
+        {
             ImGui::SetNextWindowPos(ImVec2(15, 10));
             ImGui::SetNextWindowSize(ImVec2(editor_size_x, editor_size_y));
             //ImGui::SetNextWindowFocus();
             ImGui::Begin("SOURCE CODE", __null, ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoCollapse);
-            
+
             if(ImGui::BeginMenuBar())
             {
                 if(ImGui::BeginMenu("File"))
-                {        
+                {
                     if(ImGui::MenuItem("New", "Ctrl+N"))
                     {
                         is_clicked_NEW = true;
@@ -282,14 +286,13 @@ int main(int, char**)
 
                     ImGui::EndMenu();
                 }
-                
+
                 ImGui::EndMenuBar();
             }
 
             // Writing file name instead of absoulte path
-            int position = filename.find_last_of('/');
-            position = position == filename.npos ? 0 : position + 1;
-            ImGui::Text("%s", (filename.substr(position)).c_str());
+            auto position = filename.find_last_of('/');
+            ImGui::Text("%s", filename.c_str() + (position == std::string::npos ? 0 : position + 1));
             ImGui::SameLine();
             ImGui::Text("%s", (unsaved ? "*" : ""));
 
@@ -297,22 +300,13 @@ int main(int, char**)
 
             editor.Render("Source Code Editor");
             buffer = editor.GetText();
-            
+
             if(written != buffer.length())
                 unsaved = true;
 
-            
+
             ImGui::End();
         }
-
-        //*******************
-        //JUMP TO SELECTED NODE
-        //*******************
-        if(io.KeyShift && io.KeyCtrl && io.KeysDown['F'])
-        {
-            graph.focus_node(editor.GetSelectedText());
-        }
-
         //*******************
         //NEW BUTTON WINDOW
         //*******************
@@ -363,14 +357,15 @@ int main(int, char**)
                     buffer.append(_str);
                     buffer.append("\n");
                 }
-                
-                graph.BuildCallgraphFromSource(buffer);
+
+                //graph.BuildCallgraphFromSource(buffer);
+                should_build_callgraph = true;
                 editor.SetText(buffer);
 
                 file = ".";
                 write = false;
             }
-        }    
+        }
 
         //*******************
         //SAVE BUTTON WINDOW
@@ -390,7 +385,7 @@ int main(int, char**)
             {
                save(filename.c_str(), buffer);
                bt_Save = false;
-               unsaved = false; 
+               unsaved = false;
             }
             else
             {
@@ -420,7 +415,7 @@ int main(int, char**)
                     else
                     {
                         save_prompt = true;
-                    }  
+                    }
                 }
             }
         }
@@ -430,7 +425,7 @@ int main(int, char**)
             ImGui::SetNextWindowSize(ImVec2(200, 90));
             if(ImGui::Begin("###save_prompt", &save_prompt))
             {
-                
+
                 ImGui::Text("Do you want to overwrite?");
 
                 if(ImGui::Button("OK"))
@@ -456,16 +451,79 @@ int main(int, char**)
             }
         }
 
-        if(editor.SecondsSinceLastTextChange() == 2 && should_build_callgraph)
-        {
-            graph.BuildCallgraphFromSource(buffer);
-            should_build_callgraph = false;
-        }
-
-        if(editor.IsTextChanged())
-        {
+        if(editor.IsTextChanged()) {
             should_build_callgraph = true;
         }
+    }
+};
+
+};
+
+
+
+int main(int, char**)
+{
+    gui::MainWindow main_window;
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->AddFontFromFileTTF("imgui_util/misc/fonts/Cousine-Regular.ttf", 15.0f);
+
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
+
+    gui::SourceCodePanel source_code_panel(io, main_window);
+    GraphGui::GraphGui graph(&io, &source_code_panel.Editor());
+    while (!glfwWindowShouldClose(main_window.Window()))
+    {
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::PushClipRect(ImVec2(100, 100), ImVec2(200, 200), true);
+
+        source_code_panel.Update();
+
+        float graph_size_x = io.DisplaySize.x*(1 - EDITOR_GRAPH_RATIO)-30;
+        float graph_size_y = io.DisplaySize.y-20;
+        float editor_size_x = io.DisplaySize.x*EDITOR_GRAPH_RATIO;
+        float editor_size_y = io.DisplaySize.y-20;
+
+        //*******************
+        //GENERATED GRAPH WINDOW
+        //*******************
+        {
+            ImGui::SetNextWindowPos(ImVec2(editor_size_x+25, 10));
+            ImGui::SetNextWindowSize(ImVec2(graph_size_x, graph_size_y));
+            ImGui::Begin("GENERATED CALLGRAPH", __null, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBringToFrontOnFocus);
+
+
+            graph.set_window(ImGui::GetCurrentWindow());
+            //PushClipRect(const ImVec2& clip_rect_min, const ImVec2& clip_rect_max, bool intersect_with_current_clip_rect);
+            graph.draw();
+            //graph.focus_node(std::string("base::base()"));
+
+            ImGui::End();
+            ImGui::PopClipRect();
+        }
+
+        if(io.KeyShift && io.KeyCtrl && io.KeysDown['F'])
+        {
+            graph.focus_node(source_code_panel.Editor().GetSelectedText());
+        }
+
+
+        if(source_code_panel.SecondsSinceLastTextChange() == 2 && source_code_panel.ShouldBuildCallgraph())
+        {
+            graph.BuildCallgraphFromSource(source_code_panel.SourceCode());
+            source_code_panel.CallGraphBuilt();
+        }
+
+        source_code_panel.Draw();
+
 
         // Rendering
         ImGui::Render();
